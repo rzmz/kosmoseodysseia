e4f2a9f8d3ac027777957c6db8f811dc0c1080ef
Rasmus Piho <rasmus.piho@gmail.com>
2013-04-24 20:51:31 +0300

    Initial commit with previous code

---
+++ src/Peaklass.java
@@ -0,0 +1,10 @@
+
+public class Peaklass {
+	// See on peameetod, mis kutsutakse välja programmi käivitumisel
+	public static void main(String[] args) {
+		System.out.println("Tere tulemast, kartmatu piloot!");
+		System.out.println("Mängu eesmärgiks on tunda kosmoseteemalist teooriat ning saata rakett kuule.");
+		KosmoseOdysseia mang = new KosmoseOdysseia();
+		mang.Alusta();
+	}
+}
