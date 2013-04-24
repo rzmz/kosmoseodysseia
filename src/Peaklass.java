93ef813dfa19da2f22ce3a0aa9c1bb97d02fe868
Juhan-Rasmus Risti <juhan@risti.eu>
2013-04-24 21:07:49 +0300

    commit

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,9 +1,9 @@
 
 public class Peaklass {
-	// See on peameetod, mis kutsutakse v√§lja programmi k√§ivitumisel
+	// See on peameetod, mis kutsutakse v‰lja programmi k‰ivitumisel
 	public static void main(String[] args) {
 		System.out.println("Tere tulemast, kartmatu piloot!");
-		System.out.println("M√§ngu eesm√§rgiks on tunda kosmoseteemalist teooriat ning saata rakett kuule.");
+		System.out.println("M‰ngu eesm‰rgiks on tunda kosmoseteemalist teooriat ning saata rakett kuule.");
 		KosmoseOdysseia mang = new KosmoseOdysseia();
 		mang.Alusta();
 	}
